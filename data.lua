72ae34b884e00680325a88a026a6b79dbd0794ee
Gianmarco Venturi <32395098+TheBrutalX@users.noreply.github.com>
2024-05-01 16:30:40 +0200

    Add files via upload

---
+++ data.lua
@@ -0,0 +1,16 @@
+data:extend({
+  {
+    type = "virtual-signal",
+    name = "sts_arrow_in",
+    icon = "__simple-station-signals__/graphics/arrow-in.png",
+    icon_size = 64,
+    subgroup = "virtual-signal"
+  },
+  {
+    type = "virtual-signal",
+    name = "sts_arrow_out",
+    icon = "__simple-station-signals__/graphics/arrow-out.png",
+    icon_size = 64,
+    subgroup = "virtual-signal"
+  }
+})
